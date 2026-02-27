f3b5ddf9a79b85d46960b9d6ce70b666b1978987
Joan Alvarez <joanquique@gmail.com>
2026-02-27 17:56:03 -0600

    se agrega url de front

--- config/settings.py
+++ config/settings.py
@@ -129,4 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://hound-express-front.netlify.app/",
 ]
